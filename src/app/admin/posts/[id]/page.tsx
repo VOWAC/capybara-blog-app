0d051a7e960829bbcaa6c2796ac67028f178b677
VOWAC <yukihakanai.2@gmail.com>
2024-10-03 22:26:12 +0900

    詳細ページの書き換え

--- src/app/admin/posts/[id]/page.tsx
+++ src/app/admin/posts/[id]/page.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import { notFound } from 'next/navigation';
+import { createClient } from '@/utils/supabase/server';
 
-const PostPage = ({ params }: { params: { id: string } }) => {
+export default async function AdminPostDetail({ params }: { params: { id: string } }) {
+  const supabase = createClient();
+
+  const { data: user, error: authError } = await supabase.auth.getUser();
+
+  if (authError || !user) {
+    notFound();
+  }
+
+  
+  const { data: post, error: postError } = await supabase
+    .from('posts')
+    .select('*')
+    .eq('id', params.id)
+    .single();
+
+  if (postError || !post) {
+    notFound();
+  }
+
   return (
-    <div className="container mx-auto py-5">
-      <p className="text-[50px]">{params.id}</p>
+    <div>
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
+      <p>{post.is_published ? '公開' : '非公開'}</p>
+      <p>作成日: {new Date(post.created_at).toLocaleDateString()}</p>
     </div>
   );
-};
-
-export default PostPage;
+}
