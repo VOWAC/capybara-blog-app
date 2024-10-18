c0a9e9f9acd5152a325c881829ac5167084bdbd5
VOWAC <yukihakanai.2@gmail.com>
2024-10-18 23:47:59 +0900

    タイトルの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
+import Title from "./components/Title";
 
 export default function Home() {
   return (
-    <div>
+    
       <div>
         <Header />
         <h1>カピバラブログ</h1>
+        <Title />
       </div>
-    </div>
   );
 }
