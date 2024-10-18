5d98d2a87c46346f7be9df162495d92199e83848
VOWAC <yukihakanai.2@gmail.com>
2024-10-18 22:41:55 +0900

    背景色の設定

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='h-14 md:h-24 bg-gradient-to-r from-primary to-secondary border border-foreground shadow'>Header</div>
   )
 }
 
 export default Header
+
