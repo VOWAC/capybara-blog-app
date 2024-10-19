fb73d249a1701f2b38009d6470c305e6b95afbe0
VOWAC <yukihakanai.2@gmail.com>
2024-10-20 04:42:07 +0900

    記事詳細ページのスタイル調整

--- src/app/admin/posts/[id]/page.tsx
+++ src/app/admin/posts/[id]/page.tsx
@@ -1,7 +1,13 @@
-import { notFound } from 'next/navigation';
-import { createClient } from '@/utils/supabase/server';
+import { notFound } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
+import Link from "next/link";
 
-export default async function AdminPostDetail({ params }: { params: { id: string } }) {
+export default async function AdminPostDetail({
+  params,
+}: {
+  params: { id: string };
+}) {
   const supabase = createClient();
 
   const { data: user, error: authError } = await supabase.auth.getUser();
@@ -10,11 +16,10 @@ export default async function AdminPostDetail({ params }: { params: { id: string
     notFound();
   }
 
-  
   const { data: post, error: postError } = await supabase
-    .from('posts')
-    .select('*')
-    .eq('id', params.id)
+    .from("posts")
+    .select("*")
+    .eq("id", params.id)
     .single();
 
   if (postError || !post) {
@@ -22,11 +27,33 @@ export default async function AdminPostDetail({ params }: { params: { id: string
   }
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-      <p>{post.is_published ? '公開' : '非公開'}</p>
-      <p>作成日: {new Date(post.created_at).toLocaleDateString()}</p>
+    <div className="">
+      <div className="bg-white mx-96 px-12 py-2.5 rounded-sm my-24">
+        <div className="flex justify-between items-center">
+          <p className="small-text bg-primary w-20 flex justify-center items-center rounded-xl text-white py-1 mt-4">
+            {post.is_published ? "公開中" : "非公開"}
+          </p>
+          <button>
+            <Link href="./">
+              <Image
+                src={"/icons/edit.svg"}
+                alt="編集"
+                width={30}
+                height={30}
+                className=""
+              />
+            </Link>
+          </button>
+        </div>
+        <div className="mt-2.5 mb-9">
+          <p className="small-text opacity-50">
+            {new Date(post.created_at).toLocaleDateString()}
+          </p>
+          <h1 className="font-bold text-center mt-10 mb-6">{post.title}</h1>
+          <div className="w-full border-t-2 border-accent opacity-50"></div>
+          <p className="mt-9">{post.content}</p>
+        </div>
+      </div>
     </div>
   );
 }
