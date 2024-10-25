cfa51d44a3f103d49a1eaab4beecd3acbcd55cef
VOWAC <yukihakanai.2@gmail.com>
2024-10-25 21:09:11 +0900

    アバウトページの変更

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,41 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import Title from "../components/Title";
 import Image from "next/image";
 import Link from "next/link";
+import { getProfile } from "@/utils/aboutActions";
 
-const avatarUrl = `${process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL_AVATAR}`|| '';
-const midorinUrl = `${process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL_MIDORIN}`|| '';
+const avatarUrl =
+  `${process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL_AVATAR}` || "";
+const midorinUrl =
+  `${process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL_MIDORIN}` || "";
 
-const About = () => {
+const AboutPage = () => {
+  const [bio, setBio] = useState("");
+  const [xUrl, setXUrl] = useState("");
+  const [youtubeUrl, setYoutubeUrl] = useState("");
+  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [ruby, setRuby] = useState("");
+
+  // ページ読み込み時にプロフィールデータを取得してフォームにセットする
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const { data, error } = await getProfile(); // プロフィール取得に変更
+      if (error || !data) {
+        setMessage("プロフィールが見つかりません。");
+        return;
+      }
+      setBio(data.bio);
+      setXUrl(data.x_url);
+      setYoutubeUrl(data.youtube_url);
+      setName(data.name);
+      setRuby(data.ruby);
+    };
+
+    fetchProfile();
+  }, []);
   return (
     <div>
       <Header />
@@ -28,8 +56,8 @@ const About = () => {
 
               <div className="bg-white w-[450px] h-16 flex justify-center items-center">
                 <ruby>
-                  名前名前
-                  <rt>なまえなまえ</rt>
+                  {name}
+                  <rt>{ruby}</rt>
                 </ruby>
               </div>
             </div>
@@ -44,19 +72,25 @@ const About = () => {
                   height={72}
                 />
               </Link>
-              <Link href="https://x.com/">
+              <Link href={`${xUrl}`}>
                 <Image src={"/about/x.png"} alt="x" width={72} height={72} />
               </Link>
             </div>
             <p className="px-24 pb-24">
-              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+              {bio}
             </p>
           </div>
-          <Image src={midorinUrl} alt="midorin" width={500} height={500} className="relative bottom-[450px] left-[450px]"/>
+          <Image
+            src={midorinUrl}
+            alt="midorin"
+            width={500}
+            height={500}
+            className="relative bottom-[450px] left-[450px]"
+          />
         </div>
       </div>
     </div>
   );
 };
 
-export default About;
+export default AboutPage;
