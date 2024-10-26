72d64cf59a68fcce41cb7ff47ba679ad05425454
VOWAC <yukihakanai.2@gmail.com>
2024-10-27 04:23:07 +0900

    homeの設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,44 @@
-import Button from "./components/Button";
-import Card from "./components/Card";
-import Header from "./components/Header";
-import Title from "./components/Title";
+import Card from "@/app/components/Card";
+import Title from "@/app/components/Title";
+import { createClient } from "@/utils/supabase/server";
+import Link from "next/link";
+import Header from "@/app/components/Header";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+
+  const { data: posts, error: postsError } = await supabase
+    .from("posts")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (postsError || !posts) {
+    return <p>記事が見つかりません。</p>;
+  }
+
   return (
     <div>
-      <div>
-        <Header />
-        <h1>カピバラブログ</h1>
-        <Title text="カピバラブログ"/>
-        <Card date="2024/10/19" title="タイトルタイトルタイトルタイトルタイトルタイトルタイトル"/>
-        <Button>記事を作成</Button>
+      <Header />
+      <div className="ml-80 my-20">
+        <Title text="記事一覧" />
+      </div>
+      <div className="flex flex-col items-center">
+        {posts.length === 0 ? (
+          <h2>記事がありません。</h2>
+        ) : (
+          <ul className="grid grid-cols-1 md:grid-cols-2 gap-x-14 gap-y-5">
+            {posts.map((post) => (
+              <li key={post.id}>
+                <Link href={`/admin/posts/${post.id}`}>
+                  <Card
+                    title={post.title}
+                    date={new Date(post.created_at).toLocaleDateString()}
+                  />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
