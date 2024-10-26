37724f1e4c001ea78ad987c2f4c7d9ea6ef63d75
VOWAC <yukihakanai.2@gmail.com>
2024-10-27 04:28:30 +0900

    パスの変更

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Header from "../components/Header";
-import Title from "../components/Title";
+import Header from "@/app/components/Header";
+import Title from "@/app/components/Title";
 import Image from "next/image";
 import Link from "next/link";
 import { getProfile } from "@/utils/aboutActions";
