9645c3795d2d602cf0bd58257abd75fc5ea37325
VOWAC <yukihakanai.2@gmail.com>
2024-10-27 14:06:27 +0900

    記事詳細ページのレスポンシブ

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -59,39 +59,41 @@ export default function AdminPostDetail({
   return (
     <div>
       <Header />
-      <div className="absolute left-12 top-32">
-        <ActionButton type="back" />
-      </div>
-      <div className="bg-white mx-96 px-12 py-2.5 rounded-sm my-24">
-        {isAdmin && (
-          <div className="flex justify-between items-center">
-            <p className="small-text bg-primary w-20 flex justify-center items-center rounded-xl text-white py-1 mt-4">
-              {post.is_published ? "公開中" : "非公開"}
-            </p>
-            <ActionButton type="edit" postId={params.id} />
-          </div>
-        )}
-        <div className="mt-2.5 mb-9">
-          <p className="small-text opacity-50">
-            {new Date(post.created_at).toLocaleDateString()}
-          </p>
-          <h1 className="font-bold text-center mt-10 mb-6">{post.title}</h1>
-          <div className="w-full border-t-2 border-accent opacity-50"></div>
-          <p className="mt-9">{post.content}</p>
+      <div className="container my-24 md:my-48">
+        <div className="absolute left-12 top-32 hidden md:block">
+          <ActionButton type="back" />
         </div>
-      </div>
-      <div className="w-3/5 mx-auto">
-        <div className="flex justify-between">
-          <PaginationButton
-            type="prev"
-            currentId={params.id}
-            isAdmin={isAdmin}
-          />
-          <PaginationButton
-            type="next"
-            currentId={params.id}
-            isAdmin={isAdmin}
-          />
+        <div className="bg-white w-full px-4 md:px-12 py-1 md:py-2.5 rounded-sm md:my-24">
+          {isAdmin && (
+            <div className="flex justify-between items-center">
+              <p className="small-text bg-primary w-20 flex justify-center items-center rounded-xl text-white py-1 mt-4">
+                {post.is_published ? "公開中" : "非公開"}
+              </p>
+              <ActionButton type="edit" postId={params.id} />
+            </div>
+          )}
+          <div className="mt-2.5 mb-9">
+            <p className="small-text opacity-50">
+              {new Date(post.created_at).toLocaleDateString()}
+            </p>
+            <h1 className="font-bold text-center mt-2 md:mt-10 mb-2 md:mb-6">{post.title}</h1>
+            <div className="w-full border-t-2 border-accent opacity-50"></div>
+            <p className="mt-9">{post.content}</p>
+          </div>
+        </div>
+        <div className="w-full mx-auto mt-8 md:mt-12">
+          <div className="flex flex-col md:flex-row justify-between gap-y-4 items-center">
+            <PaginationButton
+              type="next"
+              currentId={params.id}
+              isAdmin={isAdmin}
+            />
+            <PaginationButton
+              type="prev"
+              currentId={params.id}
+              isAdmin={isAdmin}
+            />
+          </div>
         </div>
       </div>
     </div>
